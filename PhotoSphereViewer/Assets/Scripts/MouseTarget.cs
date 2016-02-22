57f90e1eabf90fbf973c0d0ca01527ae2b9dd46d
Sayri <sayris@uchicago.edu>
2016-02-22 14:36:34 -0600

    pointer added, some other wonky things

---
+++ PhotoSphereViewer/Assets/Scripts/MouseTarget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MouseTarget : MonoBehaviour {
+	public SpriteRenderer targetSprite;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void TargetChosen(bool chosen){
+		if (chosen) {
+			targetSprite.color = Color.white;
+		} else {
+			targetSprite.color = Color.red;
+		}
+	}
+}
