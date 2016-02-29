0261e99581eeaa34a9a260cbd1d0f680526890b6
bobni <bobby.l.ni@gmail.com>
2016-02-29 14:03:42 -0600

    audio

---
+++ PhotoSphereViewer/Assets/Scripts/MicrophoneRecord.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	void Start() {
+		Debug.Log ("here");
+		foreach (string device in Microphone.devices) {
+			Debug.Log("Name: " + device);
+		}
+	}
+}
