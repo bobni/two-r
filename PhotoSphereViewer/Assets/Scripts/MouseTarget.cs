8ee8fbb4983a27a4394d163c7c71649052d02165
Sayri <sayris@uchicago.edu>
2016-02-29 14:17:34 -0600

    pointer is more or less working

--- PhotoSphereViewer/Assets/Scripts/MouseTarget.cs
+++ PhotoSphereViewer/Assets/Scripts/MouseTarget.cs
@@ -4,18 +4,38 @@ using System.Collections;
 
 public class MouseTarget : MonoBehaviour {
 	public SpriteRenderer targetSprite;
+	public Object obj;
+	private static MouseTarget mInstance;
+	public static MouseTarget instance {
+		get { return mInstance; }
+	}
+	private bool objSelected;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		if (mInstance == null) {
+			mInstance = this;
+		}
+		objSelected = false;
 	}
-	
+
 	public void TargetChosen(bool chosen){
+		objSelected = chosen;
 		if (chosen) {
-			targetSprite.color = Color.white;
-		} else {
 			targetSprite.color = Color.red;
+			Debug.Log ("mouse target is chosen");
+		} else {
+			targetSprite.color = Color.white;
+			Debug.Log ("mouse target is not chosen");
+		}
+	}
+
+	public void instantiateObj() {
+		Debug.Log ("instatiateObj() called");
+		Vector3 position = transform.position;
+		if (!objSelected) {
+			Instantiate (obj, position, Quaternion.identity);
 		}
 	}
 }
---
+++ PhotoSphereViewer/Assets/Scripts/TestReaction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class TestReaction : MonoBehaviour {
+	private CardboardHead head;
+	private Vector3 startingPosition;
+	public Object newObj;
+	public SpriteRenderer sRender;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		startingPosition = transform.localPosition;
+		SetGazedAt(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private Vector3 calcRandomDistance() {
+		Vector3 direction = Random.onUnitSphere;
+		direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
+		direction.z = Mathf.Clamp (direction.z, 0.4f, 17f);
+		float distance = 2 * Random.value + 1.5f;
+		return direction * distance;
+	}
+
+	public void CreateNewObject () {
+		Vector3 position = calcRandomDistance ();
+		Instantiate (newObj, position, Quaternion.identity);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		Debug.Log ("hmmmm");
+		if (MouseTarget.instance != null) {
+			MouseTarget.instance.TargetChosen (gazedAt);
+		} else {
+			Debug.Log ("mouse target is null");
+		}
+		sRender.color = gazedAt ? Color.green : Color.red;
+	}
+}
