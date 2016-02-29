35628b1781e8b8ffa94a7792b816e901c05a5184
bobni <bobby.l.ni@gmail.com>
2016-02-29 15:37:50 -0600

    object creation

---
+++ PhotoSphereViewer/Assets/createRecording.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createRecording : MonoBehaviour {
+	public GameObject recordObject;
+	public GameObject pointer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void createRecordObject () {
+		Debug.Log ("creating note");
+		Instantiate(recordObject, pointer.transform.position+(pointer.transform.forward*9), pointer.transform.rotation);
+	}
+}
