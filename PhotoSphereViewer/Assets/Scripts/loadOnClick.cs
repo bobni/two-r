233d38add747a70867fc57a336a5057443d55220
bobni <bobby.l.ni@gmail.com>
2016-03-07 16:29:30 -0600

    added menu

---
+++ PhotoSphereViewer/Assets/Scripts/loadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadOnClick : MonoBehaviour {
+
+	//public GameObject loadingImage;
+
+	public void LoadScene(int level)
+	{
+		//loadingImage.SetActive(true);
+		Application.LoadLevel(level);
+	}
+}
